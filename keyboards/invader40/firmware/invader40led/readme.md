2ff5604acc4e08bbc3d0b86fa55df26c25245e7f
tate9683 <fktatey@gmail.com>
2024-07-20 18:09:54 +0900

    up

--- keyboards/invader40/firmware/invader40led/readme.md
+++ keyboards/invader40/firmware/invader40led/readme.md
@@ -10,11 +10,11 @@
 
 Make example for this keyboard (after setting up your build environment):
 
-    make invader40led/vial:default
+    make invader40:default
 
 Flashing example for this keyboard:
 
-    make invader40led/vial:flash
+    make invader40:default:flash
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
 
@@ -25,28 +25,3 @@ Enter the bootloader in 3 ways:
 * **Bootmagic reset**: Hold down the key at (0,0) in the matrix (usually the top left key or Escape) and plug in the keyboard
 * **Physical reset button**: Briefly press the button on the back of the PCB - some may have pads you must short instead
 * **Keycode in layout**: Press the key mapped to `QK_BOOT` if it is available
-
-
-/*
- * MIT License
- *
- * © 2024 taco9683
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
--- keyboards/invader40/firmware/invader40led/vial/config.h
+++ keyboards/invader40/firmware/invader40led/vial/config.h
@@ -18,19 +18,40 @@
 /* disable print */
 //#define NO_PRINT
 
-
-#ifdef RGBLIGHT_ENABLE
-#define RGBLIGHT_LAYERS // レイヤーとの連動機能の有効化
-#define RGBLIGHT_MAX_LAYERS 6 // 連動するレイヤー数(最大8)
 #define WS2812_DI_PIN GP15
 #define WS2812_TIMING 1250
 #define WS2812_T1H 650
 #define WS2812_T0H 350
-#define WS2812_TRST_US 100
+#define WS2812_TRST_US 100 
+
+#define RGBLIGHT_LAYERS // レイヤーとの連動機能の有効化
 
 #define RGBLED_NUM 5
-/* The LEDs on the slave half go in reverse order 個々のLED番号のお知らせ*/
+
+#define RGBLIGHT_MAX_LAYERS 7 // 連動するレイヤー数(最大8)
+
+#define RGBLIGHT_LIMIT_VAL 100
+#define RGBLIGHT_DISABLE_KEYCODES
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD
+
+
+//RGBMATRIX
+// Enable RGB Matrix
+#ifdef RGB_MATRIX_ENABLE
 #define RGBLIGHT_LED_MAP { 0, 1, 2, 3, 4 }
+#    define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+#    define RGB_DISABLE_AFTER_TIMEOUT 0  // number of minutes to wait until rgb automatically turns off
+#    define RGB_DISABLE_WHEN_USB_SUSPENDED true  // turn off effects when suspended
+#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#    define RGB_MATRIX_LED_PROCESS_LIMIT 4
+#    define RGB_MATRIX_LED_FLUSH_LIMIT 26
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150
+#    define DRIVER_LED_TOTAL 35  // Change this to the number of LEDs on your keyboard
+#endif
+/* The LEDs on the slave half go in reverse order 個々のLED番号のお知らせ*/
+/*
+
 #define RGBLIGHT_LIMIT_VAL 100
 #define RGBLIGHT_DISABLE_KEYCODES
 #define RGBLIGHT_EFFECT_SNAKE
@@ -43,9 +64,16 @@
 #define RGBLIGHT_EFFECT_TWINKLE
 #define RGBLIGHT_EFFECT_RAINBOW_MOOD
 #endif
+*/
 
-
-
+/*
+#define LAYOUT( \
+   k00, k01, k02, k03, k04, k05, KC_NO, k07, k08, k09, k10, \
+   k11, k12, k13, k14, k15, k16, k17,   k18, k19, k20, k21, \
+   k22, k23, k24, k25, k26, k27, k28,   k29, k30, k31, k32, \
+                   k33, k34, k35, k36, k37, k38, k39, k40 \
+  ) 
+*/
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
@@ -53,5 +81,6 @@
 
 /* Use 1000hz polling */
 #define USB_POLLING_INTERVAL_MS 1
+
 
 
